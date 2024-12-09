1d341c52bf63145ffdc1cf5c5509e944e93eb57b
robinNcode <msmrobin518@gmail.com>
2024-12-09 07:30:52 +0600

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('Volt Wave Tech Backend is Running!');
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
